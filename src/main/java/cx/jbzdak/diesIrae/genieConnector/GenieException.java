e2b98df9853963064a1d83c77f1a3356b3f7eeec
jbzdak@gmail.com <jb@jb-943c400115fd.(none)>
2009-06-14 23:05:14 +0200

    Zmiany whitespace

--- src/main/java/cx/jbzdak/diesIrae/genieConnector/GenieException.java
+++ src/main/java/cx/jbzdak/diesIrae/genieConnector/GenieException.java
@@ -9,45 +9,43 @@ import org.apache.commons.lang.StringUtils;
  * User: Jacek Bzdak jbzdak@gmail.com
  */
 public class GenieException extends RuntimeException{
-    @NonNull private final int errorCode;
-    @CheckForNull private final Long detailedErrorCode;
-    @CheckForNull private final ErrorDescription errorDescription;
+   @NonNull private final int errorCode;
+   @CheckForNull private final Long detailedErrorCode;
+   @CheckForNull private final ErrorDescription errorDescription;
 
+   public GenieException(String message, int errorCode) {
+      super(message);
+      this.errorCode = errorCode;
+      this.detailedErrorCode = null;
+      this.errorDescription = null;
+   }
 
+   GenieException(String message, int errorCode, DscPointer dsc) {
+      super(message);
+      this.errorCode = errorCode;
+      detailedErrorCode = LibraryConnector.getDetailedError(dsc);
+      errorDescription = new ErrorDescription(detailedErrorCode);
 
-    public GenieException(String message, int errorCode) {
-        super(message);
-        this.errorCode = errorCode;
-        this.detailedErrorCode = null;
-        this.errorDescription = null; 
-    }
+   }
 
-    GenieException(String message, int errorCode, DscPointer dsc) {
-        super(message);
-        this.errorCode = errorCode;
-        detailedErrorCode = LibraryConnector.getDetailedError(dsc);
-        errorDescription = new ErrorDescription(detailedErrorCode);
+   GenieException(int errorCode, DscPointer dsc) {
+      super();
+      this.errorCode = errorCode;
+      detailedErrorCode = LibraryConnector.getDetailedError(dsc);
+      errorDescription = new ErrorDescription(detailedErrorCode);
+   }
 
-    }
+   public int getErrorCode() {
+      return errorCode;
+   }
 
-     GenieException(int errorCode, DscPointer dsc) {
-        super();
-        this.errorCode = errorCode;
-        detailedErrorCode = LibraryConnector.getDetailedError(dsc);
-        errorDescription = new ErrorDescription(detailedErrorCode);  
-    }
+   public Long getDetailedErrorCode() {
+      return detailedErrorCode;
+   }
 
-    public int getErrorCode() {
-        return errorCode;
-    }
-
-    public Long getDetailedErrorCode() {
-        return detailedErrorCode;
-    }
-
-    @Override
-    public String getMessage() {
-        return "Błąd Genie 2000, " + (StringUtils.isEmpty(super.getMessage())?"":"wiadomość orygnialna '" + super.getMessage() + "'")
-                + "Kod błędu ogólnego '" + errorCode + "', " + (errorDescription!=null?errorDescription:"");
-    }
+   @Override
+   public String getMessage() {
+      return "Błąd Genie 2000, " + (StringUtils.isEmpty(super.getMessage())?"":"wiadomość orygnialna '" + super.getMessage() + "'")
+              + "Kod błędu ogólnego '" + errorCode + "', " + (errorDescription!=null?errorDescription:"");
+   }
 }
