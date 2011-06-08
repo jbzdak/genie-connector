522861f43bebcd4df771bc22020df02aed7b0405
Jacek Bzdak <jbzdak@gmail.com>
2011-06-08 17:11:48 +0200

    ...

--- src/main/java/cx/jbzdak/diesIrae/genieConnector/GenieLibrary.java
+++ src/main/java/cx/jbzdak/diesIrae/genieConnector/GenieLibrary.java
@@ -32,9 +32,9 @@ import com.sun.jna.ptr.PointerByReference;
 import com.sun.jna.ptr.ShortByReference;
 import cx.jbzdak.diesIrae.genieConnector.structs.DSQuery;
 
-@SuppressWarnings({"MethodWithTooManyParameters", "UnusedReturnValue"}) interface GenieLibrary extends Library {
+@SuppressWarnings({"MethodWithTooManyParameters", "UnusedReturnValue"})
 
-
+interface GenieLibrary extends Library {
    /**
     * Inicjalizuje środowisko z danych z rejestru
     */
